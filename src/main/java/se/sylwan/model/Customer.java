5168df6fc8186d0587dce993455b10629416b837
casparsylwan <casparsylwan@gmail.com>
2020-12-26 01:00:57 +0100

    Customer prototype in model

---
+++ src/main/java/se/sylwan/model/Customer.java
@@ -0,0 +1,60 @@
+package se.sylwan.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity()
+public class Customer {
+	
+	@Id
+	private String email;
+	
+	private String password;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	public Customer() {}
+
+	public Customer(String email, String password, String firstName, String lastName) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
