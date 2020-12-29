8771ce28094a563959e9230ff19d04136c5ed703
casparsylwan <casparsylwan@gmail.com>
2020-12-29 14:55:34 +0100

    cors enable

--- src/main/java/se/sylwan/controller/CustomerController.java
+++ src/main/java/se/sylwan/controller/CustomerController.java
@@ -2,6 +2,7 @@ package se.sylwan.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import se.sylwan.security.CustomerDetailsService;
 import se.sylwan.security.CustomersDetail;
 import se.sylwan.security.JwtUtil;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/")
 public class CustomerController {
