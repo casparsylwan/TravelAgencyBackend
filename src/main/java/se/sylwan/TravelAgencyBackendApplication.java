b976607117e808c5a385aab55e790c60804211de
casparsylwan <casparsylwan@gmail.com>
2020-12-31 03:18:27 +0100

    cors

--- src/main/java/se/sylwan/TravelAgencyBackendApplication.java
+++ src/main/java/se/sylwan/TravelAgencyBackendApplication.java
@@ -18,9 +18,9 @@ public class TravelAgencyBackendApplication {
 	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
 	    final CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true);
+	    config.setAllowCredentials(false);
 	    config.setAllowedOrigins(Collections.singletonList("*"));
-	    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept","*"));
+	    config.setAllowedHeaders(Arrays.asList("*"));
 	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
 	    source.registerCorsConfiguration("/**", config);
