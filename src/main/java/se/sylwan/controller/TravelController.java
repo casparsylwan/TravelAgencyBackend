3e203a8d65654e35e54c9de393afec6eb2f01df0
casparsylwan <casparsylwan@gmail.com>
2021-01-09 00:18:27 +0100

    Delete in travel

--- src/main/java/se/sylwan/controller/TravelController.java
+++ src/main/java/se/sylwan/controller/TravelController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,5 +60,18 @@ public class TravelController {
 	{
 		travelRepositiory.save(travel);
 		return travel;
+	}
+	
+	@PostMapping("/travel/delete/{id}")
+	public void deleteTravel(@RequestBody Travel travel, @PathVariable long id)
+	{
+		if(travel.getId() == id)
+		{
+			travelRepositiory.delete(travel);
+		}
+		else
+		{
+			throw new ResourceAlreadyExsistException("Travel id is wrong: " + travel.getId());
+		}		
 	}
 }
