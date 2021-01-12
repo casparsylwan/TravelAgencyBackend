2fb38566fc86f519690e0d86a403734101b9ea19
casparsylwan <casparsylwan@gmail.com>
2021-01-12 22:25:55 +0100

    get the new orders

--- src/main/java/se/sylwan/controller/TravelController.java
+++ src/main/java/se/sylwan/controller/TravelController.java
@@ -15,15 +15,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import se.sylwan.exception.ResourceAlreadyExsistException;
 import se.sylwan.exception.ResourceNotFoundException;
 import se.sylwan.model.Airport;
+import se.sylwan.model.Customer;
 import se.sylwan.model.Plane;
 import se.sylwan.model.Seat;
 import se.sylwan.model.Travel;
 import se.sylwan.repository.AirportRepository;
+import se.sylwan.repository.CustomerRepository;
 import se.sylwan.repository.PlaneRepository;
 import se.sylwan.repository.SeatRepository;
 import se.sylwan.repository.TravelRepositiory;
@@ -44,6 +47,9 @@ public class TravelController {
 	
 	@Autowired
 	private SeatRepository seatRepository;
+	
+	@Autowired
+	private CustomerRepository customerRepository;
 	
 	@GetMapping("/airports/all")
 	public List<Airport> getAllAirports()
@@ -111,6 +117,16 @@ public class TravelController {
 		seats.forEach(seat -> travelSet.add(seat.getTravel()));
 		
 		return travelSet;
+	}
+	
+	@GetMapping("/travel/customerorder/{email}")
+	public List<Seat> getCustomersTravelByParam(@RequestParam String email)
+	{
+		
+		Customer customer = customerRepository.findById(email)
+				.orElseThrow(() -> new ResourceNotFoundException("Customer with email could not be found: " + email));
+		
+		return customer.getTravelOrders();
 	}
 	
 	@PostMapping("/travel/new")
